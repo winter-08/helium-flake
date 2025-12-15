fe67b0ed2cba05dc3f67a86fe2ad03749c09169f
winter <winter.jb@icloud.com>
2025-12-15 19:20:22 +0000

    Darwin added

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       platforms = [
         "x86_64-linux"
         "aarch64-linux"
+        "aarch64-darwin"
       ];
       forAllSystems = nixpkgs.lib.genAttrs platforms;
     in
@@ -19,108 +20,142 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
-          inherit (pkgs) lib;
+          inherit (pkgs) lib stdenv;
+          isDarwin = stdenv.isDarwin;
+          isAarch64 = stdenv.hostPlatform.isAarch64;
+
+          pname = "helium";
+          version = "0.7.4.1";
+          arch = if isAarch64 then "arm64" else "x86_64";
         in
         {
           helium =
-            let
-              pname = "helium";
-              version = "0.7.1.1";
-            in
-            pkgs.stdenv.mkDerivation {
-              inherit pname version;
+            if isDarwin then
+              pkgs.stdenv.mkDerivation {
+                inherit pname version;
 
-              src = pkgs.fetchurl {
-                url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-${
-                  if system == "aarch64-linux" then "arm64" else "x86_64"
-                }_linux.tar.xz";
-                sha256 =
-                  if system == "aarch64-linux" then
-                    "sha256-B7X+f0cDN4x0H7cTGs2RtT2wpAjdO3Qh35/vPIyOL8k="
-                  else
-                    "sha256-8YzBMQw4cZUI01zrqI9PPbIA9DC9c6fxO+mygdBGlfs=";
+                src = pkgs.fetchurl {
+                  url = "https://github.com/imputnet/helium-macos/releases/download/${version}/${pname}_${version}_${arch}-macos.dmg";
+                  sha256 = "sha256-9EEECuaiALU/LzdkrjllgUN+cHcxkDvPgyc52nouFrw=";
+                };
+
+                dontUnpack = true;
+                dontBuild = true;
+
+                installPhase = ''
+                  runHook preInstall
+
+                  mkdir -p $out/Applications
+
+                  mnt=$(mktemp -d)
+                  /usr/bin/hdiutil attach $src -nobrowse -readonly -mountpoint "$mnt"
+                  cp -r "$mnt/Helium.app" $out/Applications/
+                  /usr/bin/hdiutil detach "$mnt"
+
+                  runHook postInstall
+                '';
+
+                meta = {
+                  inherit platforms;
+                  description = "A private, fast, and honest web browser";
+                  homepage = "https://github.com/imputnet/helium-macos";
+                  license = lib.licenses.gpl3Only;
+                  mainProgram = "Helium";
+                  sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+                };
+              }
+            else
+              pkgs.stdenv.mkDerivation {
+                inherit pname version;
+
+                src = pkgs.fetchurl {
+                  url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-${arch}_linux.tar.xz";
+                  sha256 =
+                    if isAarch64 then
+                      "sha256-PuZK4RiNTe5NuXskd/SmgMU6imBNec/Rks8r2uegWtc="
+                    else
+                      "sha256-0azvPplorlQcEJq8saMllxGrQiBJBgnEdHmqTSReUt8=";
+                };
+
+                nativeBuildInputs = [
+                  pkgs.makeWrapper
+                  pkgs.autoPatchelfHook
+                  pkgs.qt6.wrapQtAppsHook
+                ];
+
+                buildInputs = [
+                  pkgs.glib
+                  pkgs.gdk-pixbuf
+                  pkgs.gtk3
+                  pkgs.nspr
+                  pkgs.nss
+                  pkgs.dbus
+                  pkgs.atk
+                  pkgs.at-spi2-atk
+                  pkgs.cups
+                  pkgs.expat
+                  pkgs.libxcb
+                  pkgs.libxkbcommon
+                  pkgs.at-spi2-core
+                  pkgs.xorg.libX11
+                  pkgs.xorg.libXcomposite
+                  pkgs.xorg.libXdamage
+                  pkgs.xorg.libXext
+                  pkgs.xorg.libXfixes
+                  pkgs.xorg.libXrandr
+                  pkgs.mesa
+                  pkgs.cairo
+                  pkgs.pango
+                  pkgs.systemd
+                  pkgs.alsa-lib
+                  pkgs.libdrm
+                  pkgs.qt6.qtbase
+                ];
+
+                # Ignore Qt5 shim, qt5webengine is unmaintained & we're using Qt6
+                autoPatchelfIgnoreMissingDeps = [
+                  "libQt5Core.so.5"
+                  "libQt5Gui.so.5"
+                  "libQt5Widgets.so.5"
+                ];
+
+                installPhase = ''
+                  runHook preInstall
+
+                  mkdir -p $out/bin $out/opt/helium
+                  cp -r ./* $out/opt/helium/
+
+                  makeWrapper $out/opt/helium/chrome-wrapper $out/bin/helium \
+                    --prefix LD_LIBRARY_PATH : "${
+                      lib.makeLibraryPath [
+                        pkgs.libGL
+                        pkgs.libva
+                      ]
+                    }"
+
+                  mkdir -p $out/share/applications
+                  cp $out/opt/helium/helium.desktop $out/share/applications/
+                  substituteInPlace $out/share/applications/helium.desktop \
+                    --replace-fail 'chromium' 'helium'
+
+                  mkdir -p $out/share/pixmaps
+                  cp $out/opt/helium/product_logo_256.png $out/share/pixmaps/helium.png
+
+                  runHook postInstall
+                '';
+
+                meta = {
+                  inherit platforms;
+                  description = "A private, fast, and honest web browser";
+                  homepage = "https://github.com/imputnet/helium-linux";
+                  license = lib.licenses.gpl3Only;
+                  mainProgram = "helium";
+                  sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+                };
               };
-
-              nativeBuildInputs = [
-                pkgs.makeWrapper
-                pkgs.autoPatchelfHook
-                pkgs.qt6.wrapQtAppsHook
-              ];
-
-              buildInputs = [
-                pkgs.glib
-                pkgs.gdk-pixbuf
-                pkgs.gtk3
-                pkgs.nspr
-                pkgs.nss
-                pkgs.dbus
-                pkgs.atk
-                pkgs.at-spi2-atk
-                pkgs.cups
-                pkgs.expat
-                pkgs.libxcb
-                pkgs.libxkbcommon
-                pkgs.at-spi2-core
-                pkgs.xorg.libX11
-                pkgs.xorg.libXcomposite
-                pkgs.xorg.libXdamage
-                pkgs.xorg.libXext
-                pkgs.xorg.libXfixes
-                pkgs.xorg.libXrandr
-                pkgs.mesa
-                pkgs.cairo
-                pkgs.pango
-                pkgs.systemd
-                pkgs.alsa-lib
-                pkgs.libdrm
-                pkgs.qt6.qtbase
-              ];
-
-              # Ignore Qt5 shim, qt5webengine is unmaintained & we're using Qt6
-              autoPatchelfIgnoreMissingDeps = [
-                "libQt5Core.so.5"
-                "libQt5Gui.so.5"
-                "libQt5Widgets.so.5"
-              ];
-
-              installPhase = ''
-                runHook preInstall
-
-                mkdir -p $out/bin $out/opt/helium
-                cp -r ./* $out/opt/helium/
-
-                makeWrapper $out/opt/helium/chrome-wrapper $out/bin/helium \
-                  --prefix LD_LIBRARY_PATH : "${
-                    lib.makeLibraryPath [
-                      pkgs.libGL
-                      pkgs.libva
-                    ]
-                  }"
-
-                mkdir -p $out/share/applications
-                cp $out/opt/helium/helium.desktop $out/share/applications/
-                substituteInPlace $out/share/applications/helium.desktop \
-                  --replace-fail 'chromium' 'helium'
-
-                mkdir -p $out/share/pixmaps
-                cp $out/opt/helium/product_logo_256.png $out/share/pixmaps/helium.png
-
-                runHook postInstall
-              '';
-
-              meta = {
-                inherit platforms;
-                description = "A lightweight music streaming service client";
-                homepage = "https://github.com/imputnet/helium-linux";
-                license = lib.licenses.gpl3Only;
-                mainProgram = "helium";
-              };
-            };
 
           default = self.packages.${system}.helium;
         }
       );
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.default);
     };
 }
