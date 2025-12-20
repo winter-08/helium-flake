0255901264f3d385c002c001429356ec34d624a4
Amaan Qureshi <git@amaanq.com>
2025-12-19 22:01:33 -0500

    helium: bump to 0.7.6.1

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           helium =
             let
               pname = "helium";
-              version = "0.7.1.1";
+              version = "0.7.6.1";
             in
             pkgs.stdenv.mkDerivation {
               inherit pname version;
@@ -36,9 +36,9 @@
                 }_linux.tar.xz";
                 sha256 =
                   if system == "aarch64-linux" then
-                    "sha256-B7X+f0cDN4x0H7cTGs2RtT2wpAjdO3Qh35/vPIyOL8k="
+                    "sha256-tkqUGCSBcUEpLrAlbJ9AtwjYSwcBchx/p5acBrp6Wrk="
                   else
-                    "sha256-8YzBMQw4cZUI01zrqI9PPbIA9DC9c6fxO+mygdBGlfs=";
+                    "sha256-RL0MMsYmcboZt7aq2R/6onLX1bTxlEbhlwB7yBb84os=";
               };
 
               nativeBuildInputs = [
